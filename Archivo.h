ceca44f5077f58140a5140ae8a3d3fc852eb4d74
azrxl <emiliano.medina.contreras@est.una.ac.cr>
2024-04-06 14:00:30 -0600

    declaracion clase archivo (singleton)

--- Archivo.h
+++ Archivo.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <fstream>
 #include "Lista.h"
+#include "Producto.h"
+#include "Factura.h"
 
-/*
+//Implementado con singleton
+
 class Archivo
 {
 public:
-	Archivo();
+	Archivo(std::string nombre)
+		: nombreArchivo{nombre} {}
 	virtual ~Archivo();
-	void guardarLista(Lista*);
+	void guardarProductos(Lista<Producto>* lista);
+	void guardarFacturas(Lista<Factura>* lista);
 	void cargarLista(std::string nombre);
 
 private:
 	std::string nombreArchivo;
 };
-
-*/
